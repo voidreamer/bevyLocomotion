f2dd25d11e5b493fc9d9aea0676ba464ec7210a9
Alejandro Cabrera <voidreamer@gmail.com>
2025-03-06 09:03:08 -0500

    .

--- src/main.rs
+++ src/main.rs
@@ -34,20 +34,17 @@ fn setup(
         }.build(),
     ));
 
-    // Ground plane
     commands.spawn((
         Mesh3d(meshes.add(Plane3d::default().mesh().size(20.0, 20.0))),
         MeshMaterial3d(materials.add(Color::linear_rgb(0.3, 0.5, 0.3))),
         Transform::from_xyz(0.0, 0.0, 0.0),
     ));
 
-    // Camera
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 
-    // Character - we'll load your GLTF model
     commands.spawn((
         SceneRoot(asset_server.load("models/character.glb#Scene0")),
         Transform::from_xyz(0.0, 0.0, 0.0),
